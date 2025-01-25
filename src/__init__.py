515ef97860ee113de25f802bb1c4c7d171946bc8
Yannick Wiggers <yannick.wiggers@kolibrigames.com>
2025-01-25 20:06:59 +0100

    Fixes sessions global

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,17 @@
 from src.Session import Session
 
-import globals
+import sessions
 
 def get_or_create_session(session_id: int) -> Session:
-    if session_id not in globals.sessions:
-        globals.sessions[session_id] = Session(session_id)
-    return globals.sessions[session_id]
+    if session_id not in sessions.sessions:
+        sessions.sessions[session_id] = Session(session_id)
+    return sessions.sessions[session_id]
 
 
 def close_session(session_id: int) -> None:
-    if session_id in globals.sessions:
-        del globals.sessions[session_id]
+    if session_id in sessions.sessions:
+        del sessions.sessions[session_id]
 
 
 def reset_sessions() -> None:
-    globals.sessions = {}
+    sessions.sessions = {}
